da83fd823685c2268c0c7ce57a8049eb2116c5a1
ArgelOrtiz <7624000argel>
2019-10-11 00:56:37 -0500

    Initial commit

---
+++ app/src/main/java/com/tec/aoamenu/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.tec.aoamenu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class DetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
